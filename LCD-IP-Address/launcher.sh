e1874036b83868d8a8874eb7c272fcb43f7aaddf
Perry Addey <perryaddey@gmail.com>
2024-11-21 02:35:33 -0500

    Update launcher.sh

--- LCD-IP-Address/launcher.sh
+++ LCD-IP-Address/launcher.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-cd /LCD-IP-Address 
+
+cd /                                  #takes you to home directory 
+cd home/user/LCD-IP-Address           #type pwd in the command line to find the path to where getipaddress.py is located
 python3 getipaddress.py 
+cd /                                  #takes you back to the home directory after the IP address is displayed
